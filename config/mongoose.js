d6cb35d4ca870f3490e7bcb4c24174beda776f41
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-12-27 13:50:45 +0200

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Use MONGODB_URI if defined, else default to localhost connection
-const mongoURI = process.env.MONGODB_URI || `mongodb://localhost/${process.env.DATABASE_NAME}`;
+const mongoURI =  "mongodb+srv://abdoarh36:TyWF4ABOefQhJFbP@cluster0.bc7sxu7.mongodb.net/TreadsApp"// process.env.MONGODB_URI || `mongodb://localhost/${process.env.DATABASE_NAME}`;
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
